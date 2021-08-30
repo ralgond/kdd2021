85844e8fb30540d41ec97053728d46bf6a51d445
ralgond <ht201509@163.com>
2021-08-30 19:05:41 +0800

    Updated

--- gen_gv_hotsax.py
+++ gen_gv_hotsax.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     for fn in os.listdir("only_test_input"):
         file_no = int(fn.split(".")[0])
         fp = os.path.join("only_test_input", fn)
+        os.system(f"mkdir -p gv_hotsax_output/{win_size}")
         os.system(f"java -cp grammarviz2_src/target/grammarviz2-0.0.1-SNAPSHOT-jar-with-dependencies.jar net.seninp.grammarviz.GrammarVizAnomaly -alg HOTSAX  -w {win_size} -p 4 -a 4 -i {fp} > gv_hotsax_output/{win_size}/{file_no}.txt")
